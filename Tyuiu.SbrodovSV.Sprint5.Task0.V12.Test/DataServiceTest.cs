6ac9ddb9a21e4bc4b522d897d8b967343dad534c
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-11-16 20:21:36 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SbrodovSV.Sprint5.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SbrodovSV.Sprint5.Task0.V12.Lib;
+using System.IO;
+
+namespace Tyuiu.SbrodovSV.Sprint5.Task0.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\Project's C#\Tyuiu.SbrodovSV.Sprint5\Tyuiu.SbrodovSV.Sprint5.Task0.V12\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}

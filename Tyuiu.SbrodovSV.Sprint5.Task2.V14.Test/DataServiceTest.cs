79a2cb7204f7bb6b5201ba4960dac0a1674c242b
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-11-17 23:06:35 +0500

    Task2

---
+++ Tyuiu.SbrodovSV.Sprint5.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.SbrodovSV.Sprint5.Task2.V14.Lib;
+
+namespace Tyuiu.SbrodovSV.Sprint5.Task2.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\Project's C#\Tyuiu.SbrodovSV.Sprint5\Tyuiu.SbrodovSV.Sprint5.Task2.V14\bin\Debug\OutPutFileTask2.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}

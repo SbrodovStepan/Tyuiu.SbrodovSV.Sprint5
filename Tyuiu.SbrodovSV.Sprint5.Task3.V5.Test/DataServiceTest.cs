c8b6a18dd70ee98231b5c9c85e42279eaa08cedc
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-11-18 14:24:28 +0500

    Task3

---
+++ Tyuiu.SbrodovSV.Sprint5.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.SbrodovSV.Sprint5.Task3.V5.Lib;
+
+namespace Tyuiu.SbrodovSV.Sprint5.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\Project's C#\Tyuiu.SbrodovSV.Sprint5\Tyuiu.SbrodovSV.Sprint5.Task3.V5\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}
---
+++ Tyuiu.SbrodovSV.Sprint5.Task3.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SbrodovSV.Sprint5.Task3.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SbrodovSV.Sprint5.Task3.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a28c94cf-fa7b-4dd6-9b6a-727c886432ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

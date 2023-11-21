5628ca546a715722da175a7d58e0193bce8378da
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-11-21 22:23:49 +0500

    Task7

---
+++ Tyuiu.SbrodovSV.Sprint5.Task7.V22.Lib/DataService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SbrodovSV.Sprint5.Task7.V22.Lib
+{
+    public class DataService : ISprint5Task7V22
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V22.txt";
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists) File.Delete(pathSaveFile);
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    File.WriteAllText(pathSaveFile, line.Replace('.','#').Replace(',', '#').Replace('!', '#').Replace('?', '#').Replace('-', '#') + Environment.NewLine);
+                }
+            }
+            return pathSaveFile;
+
+            //string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V22.txt";
+            //FileInfo fileInfo = new FileInfo(pathSaveFile);
+            //bool fileExists = fileInfo.Exists;
+            //if (fileExists) File.Delete(pathSaveFile);
+            //string strLine = "";
+            //using (StreamReader reader = new StreamReader(path))
+            //{
+            //    string line;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        for (int i = 0; i < line.Length; i++)
+            //        {
+            //            if ((line[i] != '.') && (line[i] != ',') && (line[i] != '!') && (line[i] != '?') && (line[i] != '-'))
+            //            {
+            //                strLine += line[i];
+            //            }
+            //            else
+            //            {
+            //                strLine += "#";
+            //            }
+            //        }
+            //        File.WriteAllText(pathSaveFile, strLine + Environment.NewLine);
+            //        strLine = "";
+            //    }
+            //}
+            //return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.SbrodovSV.Sprint5.Task7.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SbrodovSV.Sprint5.Task7.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SbrodovSV.Sprint5.Task7.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63cf5e1a-8120-4172-a24f-70aa7c7e9fcb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

345feba0a338c2a460ce2fcadd522988267fde4c
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-11-21 21:39:27 +0500

    Task6

---
+++ Tyuiu.SbrodovSV.Sprint5.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SbrodovSV.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    foreach (char c in line)
+                    {
+                        if (c >= 'а' && c <= 'я')
+                        {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+            return count;
+
+            //int count = 0;
+            //Regex regex = new Regex("[а-я]");
+            //using (StreamReader reader = new StreamReader(path))
+            //{
+            //    string line;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        MatchCollection matches = regex.Matches(line);
+            //        foreach (Match match in matches)
+            //        {
+            //            count += 1;
+            //        }
+            //    }
+            //}
+            //return count;
+        }
+    }
+}
